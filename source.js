e6b84b0d9dd49da805ac0829aaf070e8241bd8a4
Forestf <michalsliwa96@wp.pl>
2018-09-25 20:41:48 +0200

    Initial commit

---
+++ source.js
@@ -0,0 +1,144 @@
+
+
+
+var ruchy =0;
+var plansza;
+var zaznaczeni;
+
+function wypelnij(){
+	
+	grid =[];
+	
+	for(var i=0 ;i <32 ; i++){
+		grid[i]=[];
+
+	}
+	
+	for(var i=0 ;i< 32 ; i++){
+		
+		
+		for(var j=0 ; j<32  ; j++){
+			
+			grid[i][j] =Math.floor((Math.random() * 5) + 0);
+		}
+			
+	}
+	
+	
+	//grid[5][6] =0;
+
+
+	return grid;
+};
+
+function rysuj(){
+	
+	c = document.getElementById("myCanvas");
+	ctx = c.getContext("2d");
+
+	
+	for(var i=0 ;i< plansza.length ; i++){
+		
+		for(var j=0 ; j<plansza[i].length  ; j++){
+			ctx.fillStyle="Yellow";
+			switch(grid[i][j]){
+				
+				case 0:
+					ctx.fillStyle="Green";
+				break;
+				
+				case 1:
+					ctx.fillStyle="Red";
+				break;
+				
+				case 2:
+					ctx.fillStyle="Blue";
+				break;
+				
+				case 3:
+					ctx.fillStyle="Yellow";
+				break;
+				
+				case 4:
+					ctx.fillStyle="Orange";
+				break;
+				
+				
+				
+			};
+			ctx.fillRect(i*16,j*16,16,16);
+		}
+			
+	}
+
+	//ctx.fillStyle="Green";
+	//ctx.fillRect(32,32,32,32)
+	ctx.stroke();
+	
+	
+};
+
+function maluj(x){
+	if(x ==plansza[0][0]) return;
+	ruchy++;
+	document.getElementById("ruch").innerHTML ="Ruchy : "+ruchy;
+	
+	
+	 kolor= plansza[0][0];
+	 plansza[0][0]= x;
+	 do_pomalowania=[];
+	 temp =[0,0];
+	 do_pomalowania.push(temp);
+	 nastepny(0,0,kolor,x , do_pomalowania)
+}
+
+function nastepny(x , y ,starykolor , nowykolor , do_pomalowania){
+	
+	//plansza[x][y]=nowykolor;
+	if(x>0 && plansza[x-1][y]==starykolor){
+		var temp= [x-1 , y];
+		do_pomalowania.push(temp);
+	}
+
+	if(x<plansza.length-1 && plansza[x+1][y]==starykolor){
+		var temp= [x+1 , y];
+		do_pomalowania.push(temp);
+	}
+	
+	if(y>0 && plansza[x][y-1]==starykolor){
+		var temp= [x , y-1];
+		do_pomalowania.push(temp);
+	}
+	
+	if(y<plansza.length-1 && plansza[x][y+1]==starykolor){
+		var temp= [x , y+1];
+		do_pomalowania.push(temp);
+	}
+	
+	if(do_pomalowania.length==0) return;
+	
+	for(var i=0 ; i<do_pomalowania.length ; i++){
+		plansza[do_pomalowania[i][0] ][ do_pomalowania[i][1]] = nowykolor;
+		
+	}
+	//sleep(10);
+	//rysuj();
+	setTimeout(function(){rysuj()}, 15);
+	
+	for(var i=0 ; i<do_pomalowania.length ; i++){
+		var tempx =do_pomalowania[i][0];
+		var tempy =do_pomalowania[i][1];
+		do_pomalowania.splice(i, 1);
+		nastepny(tempx , tempy , starykolor , nowykolor , do_pomalowania);
+		//rysuj();
+	}
+}
+
+window.onload = function main(){
+
+plansza = wypelnij();
+
+rysuj(plansza);
+
+};
+
